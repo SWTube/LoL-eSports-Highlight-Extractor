b93df3c47f09df9594303244e1bd7f72e0c99325
Alegruz <alegruz@khu.ac.kr>
2020-07-06 20:37:46 +0900

    [add] - rearranged code structure, added video support
    The new code is rearranged nicely, and bounding_boxer.py now deals with video, not image

--- visual-recognition/bounding_boxer.py
+++ visual-recognition/bounding_boxer.py
@@ -6,16 +6,34 @@ import cv2 as cv
 import numpy as np
 import random
 
-def draw_bounding_box(image: np.ndarray, contours: list, contours_poly: list, bound_rect: list) -> np.ndarray:
+def draw_bounding_box(image: np.ndarray, contours: list, contours_poly: list, bound_rect: list, max_bound_rect_index: int) -> np.ndarray:
+    """
+    :param image:
+    :param contours:
+    :param contours_poly:
+    :param bound_rect:
+    :param max_bound_rect_index:
+    :return: image with bounding box drawn on it
+    """
     assert (isinstance(image, np.ndarray))
     assert (type(contours) == list)
     assert (type(contours_poly) == list)
     assert (type(bound_rect) == list)
+    assert (type(max_bound_rect_index) == int)
 
     drawing = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
 
+    # if (0 <= max_bound_rect_index < len(bound_rect)):
+    #     color = (128, 128, 0)
+    #     cv.drawContours(drawing, contours_poly, max_bound_rect_index, color)
+    #     cv.rectangle(image, (int(bound_rect[max_bound_rect_index][0]), int(bound_rect[max_bound_rect_index][1])), \
+    #                  (int(bound_rect[max_bound_rect_index][0] + bound_rect[max_bound_rect_index][2]), int(bound_rect[max_bound_rect_index][1] + bound_rect[max_bound_rect_index][3])), color, 2)
+
     for i in range(len(contours)):
-        color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+        if (i is not max_bound_rect_index):
+            color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+        else:
+            color = (255, 255, 255)
         cv.drawContours(drawing, contours_poly, i, color)
         cv.rectangle(image, (int(bound_rect[i][0]), int(bound_rect[i][1])), \
                      (int(bound_rect[i][0] + bound_rect[i][2]), int(bound_rect[i][1] + bound_rect[i][3])), color, 2)
@@ -23,6 +41,13 @@ def draw_bounding_box(image: np.ndarray, contours: list, contours_poly: list, bo
     return image
 
 def draw_results(list_of_images: list, name_of_images: list, length: int, width: int) -> None:
+    """
+    :param list_of_images:
+    :param name_of_images:
+    :param length:
+    :param width:
+    plots all the images in list_of_images
+    """
     assert (type(list_of_images) == list)
     assert (type(name_of_images) == list)
     assert (str(length).isnumeric())
@@ -33,7 +58,14 @@ def draw_results(list_of_images: list, name_of_images: list, length: int, width:
 
     plt.show()
 
-def get_bounding_box(image: np.ndarray) -> TypeVar(list, list, list):
+def get_bounding_box(image: np.ndarray) -> TypeVar(list, list, list, int):
+    """
+    :param image:
+    :return: list0, list1, list2
+    list0 is the contours,
+    list1 is the contours poly,
+    list2 is the bounding rectangle
+    """
     assert (isinstance(image, np.ndarray))
 
     im2, contours, hierarchy = cv.findContours(image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -43,12 +75,22 @@ def get_bounding_box(image: np.ndarray) -> TypeVar(list, list, list):
     for i, c in enumerate(contours):
         contours_poly[i] = cv.approxPolyDP(c, 3, True)
         bound_rect[i] = cv.boundingRect(contours_poly[i])
-        if (len(bound_rect[i]) > len(bound_rect[max_bounding_rect])):
+        if (get_size(bound_rect[i]) > get_size(bound_rect[max_bounding_rect])):
             max_bounding_rect = i
 
-    return contours, contours_poly, bound_rect;
+    return contours, contours_poly, bound_rect, max_bounding_rect;
+
+def get_size(rectangle: tuple) -> int:
+    assert (type(rectangle) == tuple)
+    assert (len(rectangle) == 4)
+
+    return abs(rectangle[0] - rectangle[2]) * abs(rectangle[1] - rectangle[3])
 
 def sobel_detect_uint8(image: np.ndarray) -> np.ndarray:
+    """
+    :param image:
+    :return: image with sobel detection done, and type converted to uint8
+    """
     assert (isinstance(image, np.ndarray))
 
     sobel_x = cv.Sobel(image, cv.CV_64F, 1, 0, ksize=5)
@@ -56,22 +98,53 @@ def sobel_detect_uint8(image: np.ndarray) -> np.ndarray:
     # set type from float64 to uint8
     sobel = np.sqrt(sobel_x ** 2 + sobel_y ** 2)
     sobel = (sobel * 255 / sobel.max()).astype(np.uint8)
-    print(type(sobel))
 
     return sobel
 
 def main() -> None:
+    """
+    loads an image and draws a bounding box on it
+    """
     images = []
     names = []
 
     # init
     cv.setUseOptimized(True)
-    start_time = cv.getTickCount()
+    # start_time = cv.getTickCount()
 
-    # load image and gray out
-    image = cv.imread("../resources/match_screenshots/2020_spring_srb_sb.png")
-    images.append(image)
-    names.append("Original Image")
+    cap = cv.VideoCapture("../resources/match_screenshots/2020_summer_drx_dyn.mp4")
+    fourcc = cv.VideoWriter_fourcc(*'XIVD')
+    out = cv.VideoWriter("output.avi", fourcc, 20.0, (1918, 1080))
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print("Skipped frame")
+            break
+
+        frame = bounding_boxer(frame)
+        out.write(frame)
+        cv.imshow("image", frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv.destroyAllWindows()
+    # Display Performance Cost
+    # end_time = cv.getTickCount()
+    # print("Performance:", (end_time - start_time) * 1000 / cv.getTickFrequency(), "ms")
+
+    # Display Results
+    # draw_results(images, names, 3, 3)
+
+def bounding_boxer(image: np.ndarray) -> np.ndarray:
+    # crop image
+    image = image[100:200, 640:1280]
+
+    # images.append(image)
+    # names.append("Original Image")
+
     gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
     # remove noise
@@ -79,8 +152,8 @@ def main() -> None:
 
     # sobel edge detection
     sobel = sobel_detect_uint8(blurred_image)
-    images.append(sobel)
-    names.append("Sobel Edge Detection")
+    # images.append(sobel)
+    # names.append("Sobel Edge Detection")
 
     # binarization
     sobel = cv.GaussianBlur(sobel, (5, 5), 0)
@@ -91,22 +164,18 @@ def main() -> None:
     horizontal_structure = cv.getStructuringElement(cv.MORPH_RECT, (100, 1))
     # Apply morphology operations
     kernel = np.ones((5, 5), np.uint8)
-    horizontal_sobel = cv.morphologyEx(cv.dilate(sobel_binarized, kernel, iterations=2), cv.MORPH_OPEN, horizontal_structure)
-    images.append(horizontal_sobel)
-    names.append("Horizontal Dilation")
+    horizontal_sobel = cv.morphologyEx(cv.dilate(sobel_binarized, kernel, iterations=4), cv.MORPH_OPEN,
+                                       horizontal_structure)
+    # images.append(horizontal_sobel)
+    # names.append("Horizontal Dilation")
 
     # bounding box
-    contours_sobel, contours_poly_sobel, bound_rect_sobel = get_bounding_box(horizontal_sobel)
-    image_sobel = draw_bounding_box(image, contours_sobel, contours_poly_sobel, bound_rect_sobel)
-    images.append(image_sobel)
-    names.append("Bounding Box")
+    contours_sobel, contours_poly_sobel, bound_rect_sobel, max_bound_rect_index_sobel = get_bounding_box(horizontal_sobel)
+    image_sobel = draw_bounding_box(image, contours_sobel, contours_poly_sobel, bound_rect_sobel, max_bound_rect_index_sobel)
+    # images.append(image_sobel)
+    # names.append("Bounding Box")
 
-    # Display Performance Cost
-    end_time = cv.getTickCount()
-    print("Performance:", (end_time - start_time) * 1000 / cv.getTickFrequency(), "ms")
-
-    # Display Results
-    draw_results(images, names, 3, 3)
+    return image_sobel
 
 if __name__ == "__main__":
     main()
