7ea4accfcf6f23c7ece5df2700943c4e64e4f46c
Luckydipper <qhrqufdlek@naver.com>
2020-07-08 14:36:07 +0900

    [fix] - region_of_interst
    find ultimate skill region, in the video.

--- ultimate-recognition/find_region_of_interest.py
+++ ultimate-recognition/find_region_of_interest.py
@@ -1,26 +1,45 @@
 import cv2 as cv
-import numpy as np
 """
-find ultimate region.
+Name : luckydipper
+team : Visual Recognition
+created : 2020.7.8 
+purpose : Find ultimate region.
 """
-
-
-"""
-video = cv.imread("preview.jpg")
-cv.imshow("left1",video[ 150:180 ,60:85 ])
-cv.imshow("left2",video[ 250:280 ,60:85 ])
-cv.imshow("left3",video[ 357:385 ,60:85 ])
-cv.imshow("left4",video[ 460:485 ,60:85 ])
-cv.imshow("left5",video[ 560:590 ,60:85 ])
-
-
-cv.imshow("right1",video[ 150:180,1835:1860 ])
-cv.imshow("right2",video[ 250:280,1835:1860 ])
-cv.imshow("right3",video[ 357:385,1835:1860 ])
-cv.imshow("right4",video[ 460:485,1835:1860 ])
-cv.imshow("right5",video[ 560:590,1835:1860 ])
-cv.waitKey(0)
-
-
 
 """
+capture = cv.VideoCapture("Video")  # read the video file 
+
+while True:
+    check = capture.get(cv.CAP_PROP_POS_FRAMES) == capture.get(cv.CAP_PROP_FRAME_COUNT) # current frame == all frame
+    if check : 
+        capture.open("Image/Star.mp4")
+        
+    ret, frame = capture.read()
+    cv.imshow("left1",frame[ 150:180 ,60:85 ])
+    cv.imshow("left2",frame[ 250:280 ,60:85 ])
+    cv.imshow("left3",frame[ 357:385 ,60:85 ])
+    cv.imshow("left4",frame[ 460:485 ,60:85 ])
+    cv.imshow("left5",frame[ 560:590 ,60:85 ])
+
+
+    cv.imshow("right1",frame[ 150:180,1835:1860 ])
+    cv.imshow("right2",frame[ 250:280,1835:1860 ])
+    cv.imshow("right3",frame[ 357:385,1835:1860 ])
+    cv.imshow("right4",frame[ 460:485,1835:1860 ])
+    cv.imshow("right5",frame[ 560:590,1835:1860 ])
+    
+    key = cv.waitKey(33)    # analyze 33ms frame
+    if key==27:             # Esc key to stop
+     break
+    
+"""
+
+def main() -> None:
+    video = cv.imread("test.jpeg")
+    cv.imshow("left1", video[150:180, 60:85])
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
+
