74e72fff7bc4bdaef2a38397a77867be4f21e054
Luckydipper <qhrqufdlek@naver.com>
2020-07-08 13:59:28 +0900

    [add] - region_of_interst
    find ultimate skill region.

---
+++ ultimate-recognition/find_region_of_interest.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import numpy as np
+"""
+find ultimate region.
+"""
+
+
+"""
+video = cv.imread("preview.jpg")
+cv.imshow("left1",video[ 150:180 ,60:85 ])
+cv.imshow("left2",video[ 250:280 ,60:85 ])
+cv.imshow("left3",video[ 357:385 ,60:85 ])
+cv.imshow("left4",video[ 460:485 ,60:85 ])
+cv.imshow("left5",video[ 560:590 ,60:85 ])
+
+
+cv.imshow("right1",video[ 150:180,1835:1860 ])
+cv.imshow("right2",video[ 250:280,1835:1860 ])
+cv.imshow("right3",video[ 357:385,1835:1860 ])
+cv.imshow("right4",video[ 460:485,1835:1860 ])
+cv.imshow("right5",video[ 560:590,1835:1860 ])
+cv.waitKey(0)
+
+
+
+"""
