25273de7a2763c42bc79d9197324936048193b7a
Lian Choi <littlecsi27@gmail.com>
2020-07-30 22:29:23 +0900

    [add] - Added visualization code for full-video analysis.
    visualize_result() function added.

--- spell-recognition/visualization.py
+++ spell-recognition/visualization.py
@@ -18,7 +18,7 @@ from types import FunctionType
 
 def save_graph(compare_function: FunctionType, original_spell_image: np.ndarray, video_frames: list) -> None:
     """
-     Make graph with compare_function's return value and save as PNG file
+    Make graph with compare_function's return value and save as PNG file
 
     Args:
         compare_function: Function of image comparison
@@ -50,9 +50,60 @@ def save_graph(compare_function: FunctionType, original_spell_image: np.ndarray,
 
     # Make Graph
     plt.plot(x_axis, y_axis)
-    plt.xlabel("Video Time")
-    plt.ylabel("Similarity")
+    plt.xlabel("Second in Game")
+    plt.ylabel("Result Data")
     plt.savefig("../result/comparison_result.png", format="png")
+
+
+def visualize_result(result_data: list) -> None:
+    """
+    Visualizes 2D list of analysis result data and
+    saves them in 'result' folder.
+
+    Args:
+        result_data: 2D list of values returned from
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    assert isinstance(result_data, list)
+
+    ## Variable Construction
+    x_data = list(range(len(result_data[0])))
+    y_data = result_data
+
+    file_name = ["Left Summoner 1 D spell", "Left Summoner 1 F spell",
+                 "Left Summoner 2 D spell", "Left Summoner 2 F spell",
+                 "Left Summoner 3 D spell", "Left Summoner 3 F spell",
+                 "Left Summoner 4 D spell", "Left Summoner 4 F spell",
+                 "Left Summoner 5 D spell", "Left Summoner 5 F spell",
+                 "Right Summoner 1 D spell", "Right Summoner 1 F spell",
+                 "Right Summoner 2 D spell", "Right Summoner 2 F spell",
+                 "Right Summoner 3 D spell", "Right Summoner 3 F spell",
+                 "Right Summoner 4 D spell", "Right Summoner 4 F spell",
+                 "Right Summoner 5 D spell", "Right Summoner 5 F spell"]
+
+    # Iterate through result_data
+    fig, axs = plt.subplots(10, 2)
+
+    for i in range(10):
+        axs[0, i].plot(x_data, y_data[i])
+        axs[0, i].set_title(file_name[i])
+    for i in range(11, 21):
+        axs[1, i].plot(x_data, -y_data[i], "tab:green")
+        axs[1, i].set_title(file_name[i])
+
+    for ax in axs.flat:
+        ax.set(xlabel="Second in Game", ylabel="Result Data")
+
+    # Hide x labels and tick labels for top plots and y ticks for right plots.
+    for ax in axs.flat:
+        ax.label_outer()
+
+    plt.savefig("../result/Game_analysis.png", format="png")
 
 
 def main():
@@ -74,7 +125,7 @@ def main():
     smite_image = cv.cvtColor(smite_image, cv.COLOR_BGR2RGB)
 
     ## Begin frame analysis
-    save_graph(lsr.compare_images_1, smite_image, frames)
+    save_graph(lsr.mean_squared_error, smite_image, frames)
 
 
 if __name__ == '__main__':
