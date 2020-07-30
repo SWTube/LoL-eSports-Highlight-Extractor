43ab9ad3d4e2e4c0555668b6a950b35bd376078d
tjswodud <42963882+tjswodud@users.noreply.github.com>
2020-07-30 13:03:01 +0900

    [update] - Update cognition_inGame.py

--- minimap/cognition_inGame.py
+++ minimap/cognition_inGame.py
@@ -3,61 +3,57 @@
 #        Team: standardization
 #  Programmer: tjswodud
 #  Start Date: 07/07/20
-# Last Update: July 26, 2020
-#     Purpose: To determine the game is (Playing) or (not Playing) through "Template Matching" of minimap and actual game video.
+# Last Update: July 30, 2020
+#     Purpose: Determine the game is (Playing) or (not Playing) and cut Full_Video not in_game
 """
 
 import cv2 as cv
 import numpy as np
+import time
 
 
-def main():
-    # variable assignment
-
-    # HighLights
-    # cap = cv.VideoCapture("D:\Image\HightLighs_LCKSpring2020_T1vsGenG.mp4")
-
-    # Full Videos
-    cap = cv.VideoCapture("D:\Image\KTvsDragon_last_edited.mp4")
-
-    # Template Image (minimap)
-    template = cv.imread("D:\Image\minimap_templ.png", cv.IMREAD_GRAYSCALE)
-    width, height = template.shape[::-1]  # width and height of minimap
-    print(width, height)
-
+def CutVideo(capture, temp):
     # fps, codec, output
-    fps = cap.get(cv.CAP_PROP_FPS)
+    fps = capture.get(cv.CAP_PROP_FPS)
     fourcc = cv.VideoWriter_fourcc(*"mp4v")
-    output = cv.VideoWriter("D:\Image\output\output" + ".mp4", fourcc, fps, (1920, 1080), 1)
+    output = cv.VideoWriter("../resources/output/output" + ".mp4", fourcc, fps, (1920, 1080), 1)
 
     # Playing Video, writing output
     while True:
-        ret, frame = cap.read()
+        ret, frame = capture.read()
         if not ret:
             break
 
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
         # template matching
-        res = cv.matchTemplate(gray_frame, template, cv.TM_CCOEFF_NORMED)
+        res = cv.matchTemplate(gray_frame, temp, cv.TM_CCOEFF_NORMED)
         loc = np.where(res >= 0.4)
 
-        if loc[::-1][0].size == 0:  # isn't playing
-            print('Not Playing')
-        else:  # playing
-            print('Playing')
+        if loc[::-1][0].size != 0:  # playing
             output.write(frame)
 
-        cv.imshow('Frame', frame)
+        # print('writing')
 
-        key = cv.waitKey(1)
+    print('Complete Writing')
 
-        if key == 27:  # If User puts 'ESC' button, video is closed.
-            break
-
-    cap.release()
+    capture.release()
     output.release()
     cv.destroyAllWindows()
+
+
+def main():
+    start_time = time.time()
+
+    # Full Videos
+    cap = cv.VideoCapture("../resources/KTvsDragon_last_edited.mp4")
+
+    # Template Image (minimap)
+    template = cv.imread("../resources/minimap_templ.png", cv.IMREAD_GRAYSCALE)
+
+    CutVideo(cap, template)
+
+    print('time:', time.time() - start_time)
 
 
 if __name__ == '__main__':
