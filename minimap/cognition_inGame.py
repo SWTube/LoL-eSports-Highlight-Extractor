84e5e31365dac3e934285763feb7a7eafe166e51
Lian Choi <littlecsi27@gmail.com>
2020-08-02 15:27:17 +0900

    [add] - cut_video() now prints progress

--- minimap/cognition_inGame.py
+++ minimap/cognition_inGame.py
@@ -15,14 +15,18 @@ import time
 def cut_video(capture, temp):
     # fps, codec, output
     fps = capture.get(cv.CAP_PROP_FPS)
+    total_frame_count = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
     fourcc = cv.VideoWriter_fourcc(*"mp4v")
     output = cv.VideoWriter("../resources/output/output" + ".mp4", fourcc, fps, (1920, 1080), 1)
 
     # Playing Video, writing output
-    while True:
+    while capture.isOpened():
         ret, frame = capture.read()
         if not ret:
             break
+
+        current_frame = int(capture.get(cv.CAP_PROP_POS_FRAMES))
+        print("{}/{} - {}%".format(current_frame, total_frame_count, round(current_frame/total_frame_count, 2)))
 
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
@@ -31,12 +35,11 @@ def cut_video(capture, temp):
         loc = np.where(res >= 0.4)
 
         if loc[::-1][0].size != 0:  # playing
-            print('writing')
+            # print('writing')
             output.write(frame)
         else:
-            print('not writing')
-
-    print('Complete Writing')
+            # print('not writing')
+            continue
 
     capture.release()
     output.release()
@@ -47,7 +50,7 @@ def main():
     start_time = time.time()
 
     # Full Videos
-    cap = cv.VideoCapture("../resources/KTvsDragon_last_edited.mp4")
+    cap = cv.VideoCapture("../resources/APK Prince vs SANDBOX Game 1 - LCK 2020 Spring Split W6D5 - APK vs SBG G1.mp4")
 
     # Template Image (minimap)
     template = cv.imread("../resources/minimap_templ.png", cv.IMREAD_GRAYSCALE)
