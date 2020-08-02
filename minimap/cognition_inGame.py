2f42429684bfe6da2e1b31dd9fecaaf5b5bcd6a2
tjswodud <42963882+tjswodud@users.noreply.github.com>
2020-08-02 15:43:25 +0900

    [update] - Update cognition_inGame.py (increase writing rate)

--- minimap/cognition_inGame.py
+++ minimap/cognition_inGame.py
@@ -3,7 +3,7 @@
 #        Team: standardization
 #  Programmer: tjswodud
 #  Start Date: 07/07/20
-# Last Update: July 30, 2020
+# Last Update: August 02, 2020
 #     Purpose: Determine the game is (Playing) or (not Playing) and cut Full_Video not in_game
 """
 
@@ -15,28 +15,32 @@ import time
 def cut_video(capture, temp):
     # fps, codec, output
     fps = capture.get(cv.CAP_PROP_FPS)
+    total_frame_count = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
     fourcc = cv.VideoWriter_fourcc(*"mp4v")
-    output = cv.VideoWriter("../resources/output/output" + ".mp4", fourcc, fps, (1920, 1080), 1)
+    output = cv.VideoWriter("../results/output" + ".mp4", fourcc, fps, (1920, 1080), 1)
 
     # Playing Video, writing output
-    while True:
+    while capture.isOpened():
         ret, frame = capture.read()
         if not ret:
             break
 
+        current_frame = int(capture.get(cv.CAP_PROP_POS_FRAMES))
+        print("{}/{} - {}%".format(current_frame, total_frame_count, round(current_frame/total_frame_count, 2)))
+
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        # template matching
-        res = cv.matchTemplate(gray_frame, temp, cv.TM_CCOEFF_NORMED)
-        loc = np.where(res >= 0.4)
+        if capture.get(cv.CAP_PROP_POS_FRAMES) % fps == 0:
+            # template matching
+            res = cv.matchTemplate(gray_frame, temp, cv.TM_CCOEFF_NORMED)
+            loc = np.where(res >= 0.4)
 
-        if loc[::-1][0].size != 0:  # playing
-            print('writing')
-            output.write(frame)
-        else:
-            print('not writing')
-
-    print('Complete Writing')
+            if loc[::-1][0].size != 0:  # playing
+                # print('writing')
+                output.write(frame)
+            else:
+                # print('not writing')
+                continue
 
     capture.release()
     output.release()
@@ -47,7 +51,7 @@ def main():
     start_time = time.time()
 
     # Full Videos
-    cap = cv.VideoCapture("../resources/KTvsDragon_last_edited.mp4")
+    cap = cv.VideoCapture("../resources/APKvsSB.mp4")
 
     # Template Image (minimap)
     template = cv.imread("../resources/minimap_templ.png", cv.IMREAD_GRAYSCALE)
