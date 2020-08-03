99e8f7dd39432392909c06ed3a4d90050e5aa6d3
Lee <littlecsi27@gmail.com>
2020-08-04 00:25:03 +0900

    [add] - Major changes to spell_main.py
    import -> Appropriately reordered.
    
    main()
    -> Changed for loop variable i to idx
    -> Deletes variables only used inside for loop for efficient memory usage
    -> Added progress status when analysing videos

--- spell-recognition/spell_main.py
+++ spell-recognition/spell_main.py
@@ -7,9 +7,10 @@
 # Last Update: July 29, 2020
 #     Purpose: Main file to run spell analysis.
 """
-import cv2 as cv
 import lol_spell_recognition as lsr
 import visualization as vs
+
+import cv2 as cv
 
 
 def main():
@@ -27,21 +28,24 @@ def main():
                   "Exhaust.png", "Flash.png", "Ghost.png", "Heal.png",
                   "Hexflash.png", "Ignite.png", "Smite.png", "Teleport.png"]
 
-    video_path = "../resources/test_video_one_minute.mp4.mp4"
+    video_path = "../resources/test_video_one_minute.mp4"
     spell_path = "../resources/summoner_spells/"
 
     ## Initialize
     # Load spell images
-    for i in range(len(spell_file)):
-        spell_image = cv.imread(spell_path + spell_file[i])
+    for idx in range(len(spell_file)):
+        spell_image = cv.imread(spell_path + spell_file[idx])
         spell_image_data.append(spell_image)
 
         # OpenCV uses BGR as its default color order for images, so convert to RGB
-        spell_image_data[i] = cv.cvtColor(spell_image_data[i], cv.COLOR_BGR2RGB)
+        spell_image_data[idx] = cv.cvtColor(spell_image_data[idx], cv.COLOR_BGR2RGB)
+
+    # Delete variable used inside for loop for efficient memory usage
+    del spell_image
 
     # Resize all spell images to 20x20
-    for i in range(len(spell_image_data)):
-        spell_image_data[i] = cv.resize(spell_image_data[i], (20, 20))
+    for idx in range(len(spell_image_data)):
+        spell_image_data[idx] = cv.resize(spell_image_data[idx], (20, 20))
 
     # Convert video to list of frames and saves them in *frames* list variable.
     frames, frame_count = lsr.video_to_list(video_path)
@@ -55,7 +59,7 @@ def main():
 
         # Compare in-game spell images to original spells and calculates similarity value for each comparison.
         for original_spell_image in spell_image_data:
-            similarity_list.append(lsr.compare_images_1(in_game_spell_image, original_spell_image))
+            similarity_list.append(lsr.compare_images_3(in_game_spell_image, original_spell_image))
 
         # Find the image with the highest similarity value.
         spell_index = similarity_list.index(max(similarity_list))
@@ -63,14 +67,33 @@ def main():
         # Append this spell image in fixed_spell_list as np.ndarray data type.
         fixed_spell_list.append(spell_image_data[spell_index])
 
+    del similarity_list
+    del spell_index
+
     ## Begin video analysis
+    frame_no = 1
+
+    print("-- Analysing Video --")
     for frame in frames:
+        print(end='\r')
+        print("Processing... {}%".format(round(frame_no * 100 / len(frames), 2)), end='')
+
         # Extract spell images from the frame.
         in_game_spells = lsr.extract_spell_images(frame)
 
         # Compare and add similarity values into list.
         for idx in range(len(in_game_spells)):
-            similarity_result[idx].append(lsr.compare_images_1(in_game_spells[idx], fixed_spell_list[idx]))
+            similarity_result[idx].append(lsr.compare_images_3(in_game_spells[idx], fixed_spell_list[idx]))
+
+        frame_no += 1
+
+    del in_game_spells
+    print()
+
+    # Save these results in csv file
+    lsr.save_result_as_csv(similarity_result)
+
+    vs.visualize_result()
 
     ## -- test -- ##
     print("-- test --")
