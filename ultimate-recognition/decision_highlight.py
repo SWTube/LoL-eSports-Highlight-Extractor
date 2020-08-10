3dd0090009c8bfd3ca57d1d1a09280a5a7c05e07
SW0000J <64199550+SW0000J@users.noreply.github.com>
2020-08-10 14:48:34 +0900

    [fix] - fixed list error

--- ultimate-recognition/decision_highlight.py
+++ ultimate-recognition/decision_highlight.py
@@ -3,7 +3,7 @@
 #        Team: visual recognition 2
 #  Programmer: SW0000J
 #  Start Date: 08/08/20
-# Last Update: August 08, 2020
+# Last Update: August 10, 2020
 #     Purpose: to decision highlight score
 """
 
@@ -27,29 +27,29 @@ def load_ultimate_cooltime_dictionary() -> dict:
     return champion_cooltime_dictionary
 
 
-def find_max_cooltime_value() -> int:
+def find_max_cooltime_value(champion_cooltime_dictionary : dict) -> int:
     """
         Find ultimate skill's maximum cool time
         Args:
-
+            champion_cooltime_dictionary : dictionary to find max value
         Returns:
             ultimate skill's maximum cool time
         Raises:
             None
     """
-    champion_cooltime_dictionary = load_ultimate_cooltime_dictionary()
+    # champion_cooltime_dictionary = load_ultimate_cooltime_dictionary()
     max_value = max(champion_cooltime_dictionary.values())
 
     return max_value
 
 
-def decision_highlight_score(champion_name : list, ultimate_is_used : list) -> float:
+def decision_highlight_score(champion_name : list, is_ultimate_used : list) -> float:
     """
         Decision_highlight score using ultimate skill's cool time
         If ultimate skill is used, sum highlight score
         Args:
             champion_name : this is str type list, use to find ultimate skill's cool time
-            ultimate_is_used : this is bool type list, to know ultimate is used
+            is_ultimate_used : this is bool type list, to know ultimate is used
         Returns:
             champion ultimate skill's cool time dictionary
         Raises:
@@ -59,11 +59,12 @@ def decision_highlight_score(champion_name : list, ultimate_is_used : list) -> f
 
     highlight_score = 0
 
-    if ultimate_is_used:
-        highlight_score += champion_cooltime_dictionary[champion_name]
+    for champion_count in range(len(champion_name)):
+        for decision_count in range(len(is_ultimate_used)):
+            if is_ultimate_used[decision_count]:
+                highlight_score += champion_cooltime_dictionary[champion_name[champion_count]]
 
     return highlight_score
-
 
 
 def main() -> None:
