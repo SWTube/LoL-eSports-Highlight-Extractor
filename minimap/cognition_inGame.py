134cfc7c85f1c4a9b0a3976f8a874f63d070b3cc
tjswodud <42963882+tjswodud@users.noreply.github.com>
2020-08-15 12:01:34 +0900

    [update] - Update cognition_inGame.py

--- minimap/cognition_inGame.py
+++ minimap/cognition_inGame.py
@@ -12,32 +12,27 @@ import numpy as np
 import time
 
 
-def cut_video(capture, temp):
+def cut_video(capture, temp, frame, frame_number) -> None:
     # fps, codec, output
     fps = int(capture.get(cv.CAP_PROP_FPS))
     total_frame_count = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
     fourcc = cv.VideoWriter_fourcc(*"mp4v")
-    output = cv.VideoWriter("D:/results/output.mp4", fourcc, fps, (1920, 1080), 1)
+    output = cv.VideoWriter("D:/results/output_2.mp4", fourcc, 30, (1920, 1080), 1)
     is_ingame = False
 
-    frame_number = 0
-    capture.set(cv.CAP_PROP_POS_FRAMES, frame_number)
-    success, frame = capture.read()
+    success = True
 
-    # Playing Video, writing output
-    while success and frame_number <= total_frame_count:
-        frame_number += fps
-        capture.set(cv.CAP_PROP_POS_FRAMES, frame_number)
-        success, frame = capture.read()
+    while capture.isOpened():
+        # Playing Video, writing output
+        if success and frame_number <= total_frame_count:
+            frame_number += fps
+            capture.set(cv.CAP_PROP_POS_FRAMES, frame_number)
+            success, frame = capture.read() # is it loaded well? (frame)
 
-        current_frame = int(capture.get(cv.CAP_PROP_POS_FRAMES))
-        percentage = int(round(current_frame / total_frame_count, 2) * 100)
-        exe_time_seconds = int((total_frame_count - current_frame) / fps)
-        minute = int(exe_time_seconds / 60)
-        second = exe_time_seconds % 60
+            current_frame = int(capture.get(cv.CAP_PROP_POS_FRAMES))
+            percentage = int(round(current_frame / total_frame_count, 2) * 100)
 
-        if current_frame % fps == 0:
-            gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # It have an Error
             width_end, height_end = gray_frame.shape  # 1080, 1920
             width_start = round(266 / 1080 * width_end)  # 266
             height_start = round(289 / 1920 * height_end)  # 289
@@ -50,33 +45,36 @@ def cut_video(capture, temp):
 
             if loc[::-1][0].size != 0:  # playing
                 is_ingame = True
-                print("[writing] {}/{} - {}%, rest {} min {} s.".format(current_frame, total_frame_count, percentage,
-                                                                        minute, second))
             else:
                 is_ingame = False
-                print("[not writing] {}/{} - {}%, rest {} min {} s.".format(current_frame, total_frame_count, percentage,
-                                                                          minute, second))
 
-        if is_ingame:
-            output.write(frame)
-        else:
-            continue
+            if is_ingame:
+                print("[writing] {}/{} - {}%".format(current_frame, total_frame_count, percentage))
+                output.write(frame)
+            else:
+                print("[not writing] {}/{} - {}%".format(current_frame, total_frame_count, percentage))
+                continue
+
 
     capture.release()
     output.release()
     cv.destroyAllWindows()
 
 
-def main():
+def main() -> None:
     start_time = time.time()
 
     # Full Videos
-    cap = cv.VideoCapture("../resources/APKvsSB.mp4")
+    cap = cv.VideoCapture("../resources/SPvsAF.mp4")
 
     # Template Image (minimap)
-    template = cv.imread("../resources/minimap_templ.png", cv.IMREAD_GRAYSCALE)
+    template = cv.imread("../resources/minimap_templ.png")
+    template = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
 
-    cut_video(cap, template)
+    frame_number = 0
+    success, frame = cap.read()
+
+    cut_video(cap, template, frame, frame_number)
 
     print('time:', time.time() - start_time)
 
